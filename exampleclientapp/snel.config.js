ff013e6ec981e331b9983b0bd1d6b6339bd3bf9a
ValentinHae <79450010+ValentinHae@users.noreply.github.com>
2022-01-26 18:56:19 +0100

    Uploading the example Snel-project

---
+++ exampleclientapp/snel.config.js
@@ -0,0 +1,6 @@
+export default {
+  port: 3000,
+  mode: "dom",
+  plugins: [],
+  extendsImportMap: [],
+};
---
+++ exampleclientapp/src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { fade } from "svelte/transition";
+  import Typeahead from "https://raw.githubusercontent.com/svelte-reusable-components/svelte-typeahead/main/Typeaheadmodule.svelte";
+
+  let name = "Hello";
+  let possibleResults = ["Result1","Result2","Result3","Result4","Result5","Result6"];
+</script>
+
+<main transition:fade>
+  <Typeahead possibleResults={possibleResults} randomButton={true}/>
+</main>
+
+<style>
+  main {
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
